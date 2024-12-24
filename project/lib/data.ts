de681b5b6d04978ea4fa5acef1d1bfd22d32bd13
IgnjatMarko <ignjatovicmarko2023@gmail.com>
2024-12-24 13:15:10 +0100

    Fourth commit

--- project/lib/data.ts
+++ project/lib/data.ts
@@ -20,31 +20,31 @@ export const skills = [
 
 export const projects = [
   {
-    title: "E-commerce Platform",
-    description: "A full-stack e-commerce platform with real-time inventory management and secure payment processing.",
-    technologies: ["Next.js", "TypeScript", "Stripe", "Prisma", "PostgreSQL"],
-    github: "https://github.com",
-    demo: "https://demo.com",
+    title: "Portfolio Website",
+    description: "A modern, responsive portfolio website showcasing projects and skills, built with Next.js and Radix-UI.",
+    technologies: ["Next.js", "TypeScript", "Tailwind CSS", "Radix-UI"],
+    github: "https://github.com/IgnjatMarko/portfolio-website/tree/main/project",
+    demo: "https://markonext.pages.dev"
   },
   {
-    title: "Task Management App",
-    description: "A collaborative task management application with real-time updates and team features.",
-    technologies: ["React", "Node.js", "Socket.io", "MongoDB"],
-    github: "https://github.com",
-    demo: "https://demo.com",
+    title: "E-commerce Retro Store",
+    description: "A full-stack e-commerce platform with responosive design for a Retro Video rental shop.",
+    technologies: ["React", "JavaScript", "SCSS", "MongoDB", "Express"],
+    github: "https://github.com/IgnjatMarko/RetroVideo",
+    demo: "https://github.com/IgnjatMarko/RetroVideo",
   },
   {
-    title: "AI Content Generator",
-    description: "An AI-powered application that generates various types of content using GPT-3.",
-    technologies: ["Next.js", "OpenAI API", "TailwindCSS", "Vercel"],
-    github: "https://github.com",
-    demo: "https://demo.com",
+    title: "Online Bible Verse Reader",
+    description: "A user-friendly online platform for reading and searching Bible verses, utilizing real-time data updates.",
+    technologies: ["React", "JavaScript", "Tailwind CSS / DaisyUI" ,"Firebase"],
+    github: "https://github.com/IgnjatMarko/",
+    demo: "https://chanceverse.pages.dev"
   },
   {
-    title: "Analytics Dashboard",
-    description: "A real-time analytics dashboard with interactive charts and data visualization.",
-    technologies: ["React", "D3.js", "Firebase", "Material-UI"],
-    github: "https://github.com",
-    demo: "https://demo.com",
-  },
+    title: "Budget Calculator App",
+    description: "A budget calculator application that saves your data in localStorage. Advanced JavaScript app using MVC architecture.",
+    technologies: ["JavaScript", "Model View Controller", "localStorage"],
+    github: "https://github.com/IgnjatMarko/BudgetCalculator",
+    demo: "https://budgetcalcul8r.netlify.app/",
+  }
 ];
